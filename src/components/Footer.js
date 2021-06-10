279512d4645a8ae04cec3f8b99fbbe37a11b197d
Your NameAlex <alehr45@gmail.com>
2021-06-09 20:18:14 -0500

    slide info

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar , Container, NavbarBrand } from "react-bootstrap";
+
+
+
+function Footer() {
+    return (
+
+        <div className="fixed-bottom">  
+        <Navbar color="dark" dark>
+            <Container>
+                <NavbarBrand></NavbarBrand>
+            </Container>
+        </Navbar>
+    </div>
+
+
+
+
+    )
+
+
+
+};
+
+export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Image, Button, Carousel } from "react-bootstrap";
 import { SocialIcon } from 'react-social-icons';
+import Footer from '../components/Footer';
 import logo from "../images/logo1.png";
 import pic1 from "../images/trueplumb-image1.jpeg";
 import pic2 from "../images/trueplumb-image2.jpeg";
@@ -42,46 +43,49 @@ function Home() {
         <Carousel.Item>
           <img className="d-block w-100" src={pic1} alt="First slide" />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Keep it clean with True Plumb</h3>
+            <p>Cleanliness is priority at our organization. We show up wearing shoe covers and use drop cloths to protect your home.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={pic2} alt="Second slide" />
 
           <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>We Take the Time</h3>
+            <p>We will make sure that everything is true and plumb before we leave. Anything worth doing is worth doing right!</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={pic3} alt="Third slide" />
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3>We Stand Behind the Work</h3>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            You shouldn't pay to worry! Upon hiring us you can depend on the quality, we offer a 90 day labor guarantee.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={pic4} alt="Fourth slide" />
           <Carousel.Caption>
-            <h3>Fourth slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Free Estimates</h3>
+            <p>We would be thrilled to come take a look at your upcoming project! Call the office to set up an estimate, sometimes we can even quote you over the phone.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={pic5} alt="First slide" />
           <Carousel.Caption>
-            <h3>Fifth slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>No Job is Too Small
+</h3>
+            <p>We can handle any plumbing related issue in your house. If you have a running toilet, drip at a shower head, or even a strange thumping sound under the house, you can depend on us to solve the problem.
+</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="d-block w-100" src={pic6} alt="First slide" />
           <Carousel.Caption>
-            <h3>Sixth slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>Mission Statement</h3>
+            <p>At True Plumb, it is our ultimate goal to forge long lasting relationships with our customers and community.  We believe this goal can be achieved by working to perfect our trade, focusing on cleanliness, and honestly by being nice to one another.
+</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
@@ -96,7 +100,7 @@ function Home() {
       <li>Sink Repair</li>
       <li>Faucet Repair</li>
       <li>Garbage Disposals</li>
-      <li>Dishwasher Installation</li>
+      <li>Water Main Replacement</li>
       <li>Remodels</li>
       <li>Complete Plumbing repairs & solutions</li>
       <li>Leak repair</li>
@@ -107,7 +111,7 @@ function Home() {
           <span>Free $15 Coupon</span>
         </Button>
       </a>
-     
+      <Footer/>
     </div>
 
     
